5a84cde9b7adb247870067c2fb2be93af9d6aed3
Priyal Patel <46074103+patel-priyal-m@users.noreply.github.com>
2025-12-12 00:02:26 -0500

    Fix case sensitivity in Snowflake connection string

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 
 
 from snowflake.snowpark.functions import col
-cnx =  st.connection("Snowflake")
+cnx =  st.connection("snowflake")
 session =  cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
